30fcd0cf63ca8faac06d3dcee2f1f2ea46f09831
Nicolas <pnwa@icloud.com>
2022-01-11 20:24:13 +0100

    Add global.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import { ThemeProvider } from "next-themes"
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+import '../styles/global.css'
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps}/>
+  const [user, setUser] = useState(null)
+  return (
+    <ThemeProvider disableTransitionOnChange={true}>
+      <Component {...pageProps}/>
+    </ThemeProvider>
+  )
 }
 
 export default App
