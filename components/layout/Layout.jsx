6f978eb00a2147ae9b2ca7fd4a876f40f2b19e95
Nicolas <pnwa@icloud.com>
2022-01-21 21:07:27 +0100

    Remove Dev warning if connected

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -3,23 +3,27 @@ import Navbar from "./navbar/Navbar"
 import Footer from "./footer/Footer"
 import Link from "next/link"
 import styles from "./Layout.module.css"
+import { useUser } from "../../contexts/user.context"
 
 // TODO Rework Layout styles
 const Layout = ({ children, title }) => {
+  const { user } = useUser()
   return (
     <div className={styles.layout_wrapper}>
       <Head>
         <title>{title ? title + " - " : null} Unicorn's Blog </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className={styles.dev_wrapper}>
-        👷 <span>Ce site est encore en développement</span> 👷
-        <p>
-          <Link href={"/dev-report"}>
-            <a>Laisser un commentaire</a>
-          </Link>
-        </p>
-      </div>
+      {!user && (
+        <div className={styles.dev_wrapper}>
+          👷 <span>Ce site est encore en développement</span> 👷
+          <p>
+            <Link href={"/dev-report"}>
+              <a>Laisser un commentaire</a>
+            </Link>
+          </p>
+        </div>
+      )}
       <Navbar active={title} />
       <main style={{ minHeight: "60vh" }}>{children}</main>
       <Footer />
