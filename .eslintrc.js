1eb51d21c443c6dd39e591601d90bd0e6e3c77fa
Nicolas <pnwa@icloud.com>
2022-01-21 16:52:13 +0100

    Change eslint to extend next

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: "eslint:recommended",
+  extends: "next",
   parserOptions: {
     ecmaVersion: 13,
     sourceType: "module",
