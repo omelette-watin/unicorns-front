fba0f00b999bc25f2b86841cde63a14fa3a8f2a1
Nicolas <pnwa@icloud.com>
2022-02-28 18:45:21 +0100

    Style last comments

--- components/dashboard/content/modules/MyLastComments.jsx
+++ components/dashboard/content/modules/MyLastComments.jsx
@@ -1,54 +1,62 @@
 import styles from "./Modules.module.css"
-import {getCommentsByUserId} from "../../../../services/comment.service"
-import {useEffect, useState} from "react"
+import { getCommentsByUserId } from "../../../../services/comment.service"
+import { useEffect, useState } from "react"
 import Loading from "../../../helpers/Loading"
-import {useUser} from "../../../../contexts/user.context"
-import Date from "../../../helpers/Date";
-import Heure from "../../../helpers/Heure";
+import { useUser } from "../../../../contexts/user.context"
+import Date from "../../../helpers/Date"
+import Heure from "../../../helpers/Heure"
+import Link from "next/link"
 
 const MyLastComments = () => {
   const [myComments, setMyComments] = useState(null)
   const { user } = useUser()
 
-  useEffect( async () => {
-    getCommentsByUserId(user._id, 1, 5)
-      .then((res) => {
-        setMyComments(res.result)
-      })
+  useEffect(async () => {
+    getCommentsByUserId(user._id, 1, 3).then((res) => {
+      setMyComments(res.result)
+    })
   }, [])
 
   return (
     <div className={styles.module}>
       <p className={styles.title}>
-        Mes derniers <span>Commentaires</span>
+        Mes derniers{" "}
+        <span>
+          <Link href={"/my/comments"}>
+            <a>Commentaires</a>
+          </Link>
+        </span>
       </p>
       {!myComments && <Loading />}
-      {myComments &&
+      {myComments && (
         <ul className={styles.comments}>
           {myComments.map((comment) => {
-            return <li key={comment._id}>
-              <p className={styles.content}>&laquo; {comment.content} &raquo;</p>
-              <p className={styles.date}>
-                {comment.modifiedAt ? (
-                  <span>
-                    Modifié le <Date dateString={comment.modifiedAt} /> à{" "}
-                    <Heure dateString={comment.modifiedAt} />
-                  </span>
-                ) : (
-                  <span>
-                    Posté le <Date dateString={comment.createdAt} /> à{" "}
-                    <Heure dateString={comment.createdAt} />
-                  </span>
-                )}
-              </p>
-              <a href={`/blog/${comment.postId}`}>Voir l'article</a>
-            </li>
+            return (
+              <li key={comment._id}>
+                <div className={styles.content}>
+                  &laquo; {comment.content} &raquo;
+                </div>
+                <div className={styles.date}>
+                  {comment.modifiedAt ? (
+                    <span>
+                      Modifié le <Date dateString={comment.modifiedAt} /> à{" "}
+                      <Heure dateString={comment.modifiedAt} />
+                    </span>
+                  ) : (
+                    <span>
+                      Posté le <Date dateString={comment.createdAt} /> à{" "}
+                      <Heure dateString={comment.createdAt} />
+                    </span>
+                  )}
+                </div>
+                <a href={`/blog/${comment.postId}`} title={"Voir l'article"} />
+              </li>
+            )
           })}
         </ul>
-      }
-
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default MyLastComments;
+export default MyLastComments
